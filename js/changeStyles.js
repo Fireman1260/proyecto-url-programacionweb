a3b6e20d160533721f2279c2d6c0f6da3a38715c
Fireman1260 <kev.villatoro1260@outlook.com>
2018-08-24 20:01:32 -0600

    Solucion del bug al momento de cambiar el alert despues de presionar un boton

--- js/changeStyles.js
+++ js/changeStyles.js
@@ -2,24 +2,30 @@
 /*ALERTA DE BIENVENIDA */
 var contador_b = 0
 $('#btn_bienvenido').click(function(){
-    removerClase();
+
+    $('.alert-info').removeClass('alert-info');
+    $('.alert-danger').removeClass('alert-danger');
+    $('.alert-dark').removeClass('alert-dark');
+
     $('.alert').addClass('alert-success');
-    if(contador_b < 3)
-        contador_b++;
+        
     if(contador_b == 0){
         $('#titulo_mensaje').text("Bienvenido? ಠ╭╮ಠ");
         $('#mensaje').text("Estoy seguro que ya te di la bienvenida. En fin, nada ha cambiado... la pagina sigue en desarrollo");
         $('#adicional').text("Prueba algun otro boton   ");
+        contador_b++;
     }
     else if(contador_b == 1){
         $('#titulo_mensaje').text("Bienvenido... ");
         $('#mensaje').text("Hola otra vez....? bienvenido a la pagina sin ningun contenido...");
         $('#adicional').text("Prueba algun otro boton   ");
+        contador_b++;
     }
     else if(contador_b == 2){
         $('#titulo_mensaje').text("... ¬_¬");
         $('#mensaje').text("Creo que ya fue sufciente... porque dices bienvenido tantas veces? ");
         $('#adicional').text("Prueba algun otro boton ");
+        contador_b++;
     }
     else {
         $('#titulo_mensaje').text("(-_-')");
@@ -30,7 +36,11 @@ $('#btn_bienvenido').click(function(){
 
 /*ALERTA DE FUEGO */
 $('#btn_burn').click(function(){
-    removerClase();
+    
+    $('.alert-success').removeClass('alert-success');
+    $('.alert-info').removeClass('alert-info');
+    $('.alert-dark').removeClass('alert-dark');
+
     $('.alert').addClass('alert-danger');
     
         $('#titulo_mensaje').text("ARE YOU MAD?!?!?!?!?! (°ロ°)");
@@ -39,8 +49,12 @@ $('#btn_burn').click(function(){
 });
 
 /*ALERTA SECRETO */
-$('#btn_secreto').click(function(){
-    removerClase();
+$('#btn_info_secreto').click(function(){
+    
+    $('.alert-success').removeClass('alert-success');
+    $('.alert-info').removeClass('alert-info');
+    $('.alert-danger').removeClass('alert-danger');
+    
     $('.alert').addClass('alert-dark');
     
         $('#titulo_mensaje').text("SECRETO! Shhhhh.... (¬‿¬)");
@@ -50,7 +64,11 @@ $('#btn_secreto').click(function(){
 
 /*ALERTA INFO */
 $('#btn_info').click(function(){
-    removerClase();
+    
+    $('.alert-success').removeClass('alert-success');
+    $('.alert-danger').removeClass('alert-danger');
+    $('.alert-dark').removeClass('alert-dark');
+    
     $('.alert').addClass('alert-info');
     
         $('#titulo_mensaje').text("¯\_(ツ)_/¯");
@@ -58,9 +76,50 @@ $('#btn_info').click(function(){
         $('#adicional').text("Prueba algun otro boton....   ");
 });
 
-function removerClase(){
-    $('.alert-success').removeClass('.alert-warning');
-    $('.alert-info').removeClass('.alert-warning');
-    $('.alert-danger').removeClass('.alert-warning');
-    $('.alert-dark').removeClass('.alert-warning');
-}
+var color = 0;
+$('#btn_secret').click(function(){
+    
+    if(color == 0){
+        $('#titulo_mensaje').css('color', 'red');
+        $('#mensaje').css('color', 'red');
+        $('#adicional').css('color', 'red');
+        color++;
+    }
+    else if(color == 1){
+        $('#titulo_mensaje').css('color', 'orange');
+        $('#mensaje').css('color', 'orange');
+        $('#adicional').css('color', 'orange');
+        color++;
+    }
+    else if(color == 2){
+        $('#titulo_mensaje').css('color', 'yellow');
+        $('#mensaje').css('color', 'yellow');
+        $('#adicional').css('color', 'yellow');
+        color++;
+    } 
+    else if(color == 3){
+        $('#titulo_mensaje').css('color', 'green');
+        $('#mensaje').css('color', 'green');
+        $('#adicional').css('color', 'green');
+        color++;
+    }
+    else if(color == 4){
+        $('#titulo_mensaje').css('color', 'LightBlue');
+        $('#mensaje').css('color', 'LightBlue');
+        $('#adicional').css('color', 'LightBlue');
+        color++;
+    }
+    else if(color == 5){
+        $('#titulo_mensaje').css('color', 'Blue');
+        $('#mensaje').css('color', 'Blue');
+        $('#adicional').css('color', 'Blue');
+        color++;
+    }
+    else {
+        $('#titulo_mensaje').css('color', 'purple');
+        $('#mensaje').css('color', 'purple');
+        $('#adicional').css('color', 'purple');
+        color = 0;
+    }
+    
+});

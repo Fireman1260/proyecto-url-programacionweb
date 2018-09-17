04ae53d2d7719858f61103b630ac276def60507d
Fireman1260 <kev.villatoro1260@outlook.com>
2018-09-17 17:18:05 -0600

    Se soluciono un Bug donde que no insertaba al localStorage la informacion y cuando comenzaba la applicacion tiraba un error

--- hero101-react/src/index.js
+++ hero101-react/src/index.js
@@ -18,17 +18,26 @@ class Heroes extends React.Component{
     }
 
     componentDidMount() {//load the local storage data after the component renders
-        var heroes = (typeof localStorage["Heroes"] !== "undefined") ? 
-                JSON.parse(localStorage.getItem("Heroes")) : localStorage.setItem("Heroes",JSON.stringify([
-            {name: "Spiderman", hero_type: "heroe", alter_ego: "Peter Parker", species:"Human Mutate", abilities: ["Genius", "Superhuman strength", "Speed", "Durability", "Agility"], first_appearance:"Amazing Fantasy #15"},
-            {name: "Deadpool", hero_type: "anti-heroe", alter_ego: "Wade Wilson", species:"Human Mutate", abilities: ["Healing Factor", "Marksman", "Swordsman", "Hand-to-hand Combat", "Teleportation"], first_appearance:"New Mutants #98"},
-            {name: "Red Hood", hero_type: "anti-heroe", alter_ego: "Jason Todd", species:"Human", abilities: ["Marksman", "Martial Artist", "Hand-to-hand Combat", "High-tech equipment", "Enhanced strength"], first_appearance:"Batman #635"},
-            {name: "Batman", hero_type: "heroe", alter_ego: "Bruce Wayne", species:"Human", abilities: ["Genius", "Superhuman strength", "Martial Artist", "Hand-to-hand Combat", "Expert detective"], first_appearance:"Detective Comics #27"},
-            {name: "Flash", hero_type: "heroe", alter_ego: "Barry Allen", species:"Metahuman", abilities: ["Immense superhuman speed", "Agility", "Stamina", "Electrokinesis", "Body vibration"], first_appearance:"Showcase #4"},
-            {name: "Superman", hero_type: "heroe", alter_ego: "Clark Kent", species:"Kryptonian", abilities: ["Superhuman strength", "Superhuman speed", "Superhuman vision", "Superhuman breath ", "Invulnerability", "Flight"], first_appearance:"Action Comics #1"},
-            {name: "Iron Man", hero_type: "heroe", alter_ego: "Tony Stark", species:"Human", abilities: ["Genius", "Agility", "Powered armor suit", "Proficient scientist", "Engineer"], first_appearance:"Tales of Suspense #39"},
-            {name: "Captain America", hero_type: "heroe", alter_ego: "Steve Rogers", species:"Human Mutate", abilities: ["Peak human strength", "Speed", "Master martial artist", "Hand-to-hand Combat", "Accelerated healing factor"], first_appearance:"	Captain America Comics #1"}
-        ]));
+        var heroes = []
+
+        if (typeof localStorage["Heroes"] !== "undefined") {
+            heroes = JSON.parse(localStorage.getItem("Heroes"));
+        }
+        else{
+            localStorage.setItem("Heroes",JSON.stringify([
+                {name: "Spiderman", hero_type: "heroe", alter_ego: "Peter Parker", species:"Human Mutate", abilities: ["Genius", "Superhuman strength", "Speed", "Durability", "Agility"], first_appearance:"Amazing Fantasy #15"},
+                {name: "Deadpool", hero_type: "anti-heroe", alter_ego: "Wade Wilson", species:"Human Mutate", abilities: ["Healing Factor", "Marksman", "Swordsman", "Hand-to-hand Combat", "Teleportation"], first_appearance:"New Mutants #98"},
+                {name: "Red Hood", hero_type: "anti-heroe", alter_ego: "Jason Todd", species:"Human", abilities: ["Marksman", "Martial Artist", "Hand-to-hand Combat", "High-tech equipment", "Enhanced strength"], first_appearance:"Batman #635"},
+                {name: "Batman", hero_type: "heroe", alter_ego: "Bruce Wayne", species:"Human", abilities: ["Genius", "Superhuman strength", "Martial Artist", "Hand-to-hand Combat", "Expert detective"], first_appearance:"Detective Comics #27"},
+                {name: "Flash", hero_type: "heroe", alter_ego: "Barry Allen", species:"Metahuman", abilities: ["Immense superhuman speed", "Agility", "Stamina", "Electrokinesis", "Body vibration"], first_appearance:"Showcase #4"},
+                {name: "Superman", hero_type: "heroe", alter_ego: "Clark Kent", species:"Kryptonian", abilities: ["Superhuman strength", "Superhuman speed", "Superhuman vision", "Superhuman breath ", "Invulnerability", "Flight"], first_appearance:"Action Comics #1"},
+                {name: "Iron Man", hero_type: "heroe", alter_ego: "Tony Stark", species:"Human", abilities: ["Genius", "Agility", "Powered armor suit", "Proficient scientist", "Engineer"], first_appearance:"Tales of Suspense #39"},
+                {name: "Captain America", hero_type: "heroe", alter_ego: "Steve Rogers", species:"Human Mutate", abilities: ["Peak human strength", "Speed", "Master martial artist", "Hand-to-hand Combat", "Accelerated healing factor"], first_appearance:"	Captain America Comics #1"}
+            ]));
+
+            heroes = JSON.parse(localStorage.getItem("Heroes"));
+        }
+
         this.setState({heroList: heroes});
       }
 

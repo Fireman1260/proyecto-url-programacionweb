3212922c949a2a4e73a2890dd873c94942e842dc
Fireman1260 <kev.villatoro1260@outlook.com>
2018-09-16 19:34:22 -0600

    Creacion de la pagina con react

--- hero101-react/src/App.js
+++ hero101-react/src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hero 101</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          A guide on how to be a superhero.
         </p>
       </div>
     );
